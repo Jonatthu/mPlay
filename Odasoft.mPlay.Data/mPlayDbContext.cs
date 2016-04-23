04d2e413aa4c6151e58e33768252eaf66c97e954
jonatthu <jonatthu@gmail.com>
2016-04-23 14:06:04 -0700

    DbContext

---
+++ Odasoft.mPlay.Data/mPlayDbContext.cs
@@ -0,0 +1,41 @@
+﻿using Odasoft.mPlay.Models;
+using Odasoft.mPlay.Models.Mappings;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odasoft.mPlay.Data
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public class mPlayDbContext : DbContext
+    {
+        public mPlayDbContext() : base("mPlayDbContext")
+        {
+            Configuration.LazyLoadingEnabled = false;
+            Configuration.ProxyCreationEnabled = false;
+        }
+
+        #region DbSet
+        //Must create a DbSet for each class that will have a table in the DB
+        public DbSet<Article> Article { get; set; }
+
+        #endregion
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+
+            modelBuilder.Properties<DateTime>()
+                .Configure(c => c.HasColumnType("datetime2"));
+
+            //Add always the mappings that you created
+            modelBuilder.Configurations.Add(new ArticleMapping());
+
+        }
+}
+}
---
+++ Odasoft.mPlay.Models.Mappings/ArticleMapping.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odasoft.mPlay.Models.Mappings
+{
+    public class ArticleMapping : EntityTypeConfiguration<Article>
+    {
+        public ArticleMapping()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.Image).IsRequired();
+            Property(x => x.Length).IsRequired();
+            Property(x => x.Price).IsRequired();
+            Property(x => x.Title).HasMaxLength(40);
+            this.ToTable("Articles");
+        }
+    }
+}
