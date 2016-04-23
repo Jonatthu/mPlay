5ad29255d1f8da43f10f5ae84f4c4a4f622829b5
jonatthu <jonatthu@gmail.com>
2016-04-23 14:21:49 -0700

    SongMapping

---
+++ Odasoft.mPlay.Models.Mappings/SongMap.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity.ModelConfiguration;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odasoft.mPlay.Models.Mappings
+{
+    public class SongMap : EntityTypeConfiguration<Song>
+    {
+        public SongMap()
+        {
+            HasKey(x => x.Id);
+            Property(x => x.SongName).IsRequired();
+            Property(x => x.SongLength).IsRequired();
+            this.ToTable("Songs");
+        }
+    }
+}
--- Odasoft.mPlay.Models/Song.cs
+++ Odasoft.mPlay.Models/Song.cs
@@ -7,6 +7,7 @@ namespace Odasoft.mPlay.Models
 {
     public class Song
     {
+        public int Id { get; set; }
         public string SongName { get; set; }
         public string SongLength { get; set; }
     }
