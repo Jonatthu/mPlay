0130ac43cc06385733abcb17292b46f6b59b8e70
pjf2180 <pjf2180@me.com>
2016-04-23 13:38:01 -0700

    models added

---
+++ Odasoft.mPlay.Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odasoft.mPlay.Models
+{
+    public class Movie : Article
+    {
+        
+    }
+}
---
+++ Odasoft.mPlay.Models/Song.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Odasoft.mPlay.Models
+{
+    public class Song
+    {
+        public string SongName { get; set; }
+        public string SongLength { get; set; }
+    }
+}
