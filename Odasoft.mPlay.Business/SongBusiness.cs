c75bf8e3edc0df6beb0ea96d7a560c0dba8179e8
Jose Manuel <alvarez.m23@hotmail.com>
2016-04-26 21:46:01 -0700

    SongBusiness Modified

--- Odasoft.mPlay.Business/SongBusiness.cs
+++ Odasoft.mPlay.Business/SongBusiness.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Odasoft.mPlay.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,55 @@ namespace Odasoft.mPlay.Business
 {
     class SongBusiness
     {
+
+        public SongBusiness()
+        {
+
+        }
+         
+        public ICollection<Song> GetSongById(int AlbumId)
+        {
+            var songs = new List<Song>();
+
+            songs.Add(new Song
+            {
+                Id = 1,
+                SongName = "Lose Yourself",
+                SongLength = "5:23",
+                AlbumId = 1
+            });
+
+            songs.Add(new Song
+            {
+                Id = 2,
+                SongName = "Not Afraid",
+                SongLength = "4:19",
+                AlbumId = 2
+            });
+
+            songs.Add(new Song
+            {
+                Id = 3,
+                SongName = "Without Me",
+                SongLength = "5:00",
+                AlbumId = 3
+            });
+
+            songs.Add(new Song
+            {
+                Id = 4,
+                SongName = "Rap God",
+                SongLength = "6:10",
+                AlbumId = 4
+            });
+
+            return songs;
+        }
+
+        /*public ICollection<Song> EditSongById(Song model)
+        {
+
+        }*/
 
     }
 }
