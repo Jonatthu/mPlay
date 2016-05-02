56e80b82d6b085a73b50f5b51415d993e13c479f
jonatthu <jonatthu@gmail.com>
2016-05-02 09:50:07 -0700

    Carrousel Fixed

--- Odasoft.mPlay.Web/wwwroot/js/site.js
+++ Odasoft.mPlay.Web/wwwroot/js/site.js
@@ -2,17 +2,13 @@
 $(document).on('click', 'a', function (e) {
   var url = $(this).attr('href');
   var classs = $(this).attr('class');
-
-  if (classs == "navR") {
+  if (classs == "navR" || url == "#myCarousel") {
     return true;
   } else {
-
     $.get(url, function (data) {
       $('#myModal .modal-body').html(data);
-    })
-
+    });
     $('#myModal').modal('toggle');
-
     return false;
   }
 });
